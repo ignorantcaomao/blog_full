569997f0e8659db653b0b83f0fa6df23917bc5db
dongjiasheng <dongjiasheng@techstar.com.cn>
2020-05-25 10:58:03 +0800

    完成评论功能的开发

--- blog_full/urls.py
+++ blog_full/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
+    path('', include('comments.urls')),
 ]
 
 
